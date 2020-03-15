a1aa876d3be931f5f0d5ce1ea83e3dea362dd1bc
Tanya19Lion <tanyatriteknitskaya@gmail.com>
2020-03-15 14:27:13 +0200

    made about section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
 $(function(){
- 
-    // let mixer = mixitup(".portfolio__gallery-wrapper");
+
+    $('.about__items_content .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+            $('.about__items_content').find('.tab-item').removeClass('active-tab').hide();
+            $('.about__items_content .tabs').find('.tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+        });
 
     var mixer = mixitup('.portfolio__gallery-wrapper');
 });
