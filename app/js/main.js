2cc1c2cb5a4964594ee8a4109f54d116db1d070f
Tanya19Lion <tanyatriteknitskaya@gmail.com>
2020-03-16 17:37:18 +0200

    made header menu

--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,16 @@ $(function(){
         $(".about__items-info").slideUp();
     })
 
+    $(".header__inner-nav").on("click", function(){
+        $(".header__menu").addClass("active");
+    })
+
+    $(".header__close-btn").on("click", function(){
+        $(".header__menu").removeClass("active");
+    })
+
+    new WOW().init();
+
     var mixer = mixitup('.portfolio__gallery-wrapper');
 
 });
