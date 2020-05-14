13823a53ba528243f22f2471115d664910ce0950
Tanya19Lion <tanyatriteknitskaya@gmail.com>
2020-05-14 13:21:23 +0300

    added focus

--- app/js/main.js
+++ app/js/main.js
@@ -8,8 +8,32 @@ $(function(){
         $(".header__menu").removeClass("active");
     });
 
+    let block = $("#achievements__inner");
+    let blockStatus = true;
+    $(window).scroll(function() {
+        let scrollEvent = ($(window).scrollTop() > (block.position().top - $(window).height()));
+        if (scrollEvent && blockStatus) { 
+            blockStatus = false;
+            
+            $('.count').each(function () {
+                $(this).prop('Counter',0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 4000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
+            });
+        }
+    });
+
+
     new WOW().init();
 
     var mixer = mixitup('.portfolio__gallery-wrapper');
 
 });
+
+
